5b4c24162bcf4392e15d9ee6fc8e6e408e078276
PhiriFrank <125193743+PhiriFrank@users.noreply.github.com>
2023-04-10 23:10:19 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,21 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+  private:
+        int top;
+        T arr[size]={0}
+        
+ public:
+      TStack(): top(-1) {}
+      bool isFull() const {return size == top -1; }
+      bool isEmpty() {return - 1== top; }
+      void push(T value){
+           if (!isFull()){
+              arr[++top]= value;
+         }
+     }
+     T get() const {return arr[top];}
+     T pop(){ return arr[top--];}
 };
 
 #endif  // INCLUDE_TSTACK_H_
